ce2da6f4b925ed7359ba8ae37c8de8e5b6e852d0
rli <rli@ROBIN-PC-PC>
2017-10-03 11:01:47 -0400

    Add project files.

---
+++ File Management System/Main_Form.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Management_System
+{
+    public partial class Main_Form : Form
+    {
+        private const int cGrip = 16;      // Grip size
+        private const int cCaption = 32;   // Caption bar height;
+
+        protected override void OnPaint(PaintEventArgs e) {
+            Rectangle rc = new Rectangle(this.ClientSize.Width - cGrip, this.ClientSize.Height - cGrip, cGrip, cGrip);
+            ControlPaint.DrawSizeGrip(e.Graphics, this.BackColor, rc);
+            rc = new Rectangle(0, 0, this.ClientSize.Width, cCaption);
+            //e.Graphics.FillRectangle(Brushes.DarkBlue, rc);
+        }
+
+        protected override void WndProc(ref Message m) 
+        {
+            if (m.Msg == 0x84) {  // Trap WM_NCHITTEST
+                Point pos = new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16);
+                pos = this.PointToClient(pos);
+                if (pos.Y < cCaption) {
+                    m.Result = (IntPtr)2;  // HTCAPTION
+                    return;
+                }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip) {
+                    m.Result = (IntPtr)17; // HTBOTTOMRIGHT
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        //Receipt parent;
+        Size Start_Size = new Size();
+        int Start_Location_Offset = 45;
+
+        public Main_Form()//Receipt _parent)
+        {
+            //this.Location = new Point(_parent.Location.X + Start_Location_Offset, _parent.Location.Y + Start_Location_Offset - 15);
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            //parent = _parent;
+            Start_Size = this.Size;
+            //Set_Form_Color(parent.Frame_Color);
+        }
+
+        private void Receipt_Load(object sender, EventArgs e)
+        {
+            // Mousedown anywhere to drag
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form_MouseDown);
+        }
+
+        private void minimize_button_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void close_button_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+        }
+
+        public void Set_Form_Color(Color randomColor)
+        {
+            //minimize_button.ForeColor = randomColor;
+            //close_button.ForeColor = randomColor;
+            textBox1.BackColor = randomColor;
+            textBox2.BackColor = randomColor;
+            textBox3.BackColor = randomColor;
+            textBox4.BackColor = randomColor;
+        }
+
+        private void Add_button_Click(object sender, EventArgs e)
+        {
+            PDFGenerator PDFG = new PDFGenerator();
+            PDFG.Generate_Invoice(SO_NUM_BOX.Text);
+        }
+    }
+}
